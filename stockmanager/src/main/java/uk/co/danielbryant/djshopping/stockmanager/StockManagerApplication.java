d60b1646a78e77a184259b6ba8a875452b13cd48
Siddu2996 <65285266+Siddu2996@users.noreply.github.com>
2020-05-13 17:04:53 +0530

    Update StockManagerApplication.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
@@ -8,5 +8,7 @@ public class StockManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockManagerApplication.class, args);
+        
+        System.out.println("welcome from github");
     }
 }
